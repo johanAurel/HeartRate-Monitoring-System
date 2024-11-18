7299d841bd43b28ca18565025550c651ae1f146c
Johan Aurel Botouli <151972603+johanAurel@users.noreply.github.com>
2024-11-18 02:50:44 +0000

    Update commands.sh

--- CE/commands.sh
+++ CE/commands.sh
@@ -2,6 +2,5 @@ python3 heart_rate_monitor/manage.py makemigrations
 
 python3 heart_rate_monitor/manage.py migrate
 
-python3 heart_rate_monitor/manage.py collect statics
+python3 heart_rate_monitor/manage.py collectstatics
 
-python3 heart_rate_monitor/manage.py runserver
